1f98c4a6e6d2079112a6ed8fa3a3bfe6b551b46a
Naganishe <mr.nordakov@mail.ru>
2024-02-02 17:28:30 +0300

    seminar1.1

--- seminars/seminar1/seminar1.js
+++ seminars/seminar1/seminar1.js
@@ -34,9 +34,45 @@ const books = [
 const library = {
   books: [...books],
   [Symbol.iterator]: function () {
-    let book = 0;
-    return { next: () => {
-        if ()
-    } };
+    let countBooks = 0;
+    return {
+      next: () => {
+        if (countBooks >= this.books.length) {
+          return { done: true };
+        } else {
+          return { value: this.books[countBooks++], done: false };
+        }
+      },
+    };
   },
 };
+
+for (const book of library) {
+  console.log(book.author, book.title);
+}
+
+//--------------------------------------------
+
+const arrayDivEl = Array.from(document.querySelectorAll("div"));
+const resArr = arrayDivEl.filter((div) => div.hasAttribute("data-active"));
+console.log(resArr);
+
+//--------------------------------------------
+
+let lessons = new Map();
+lessons.set("Математика", "Смирнов");
+lessons.set("История", "Иванов");
+
+const stanli = {
+  name: "Gorge",
+};
+
+const gorgeLessons = new Set();
+gorgeLessons.add("Математика");
+gorgeLessons.add("История");
+
+let Students = new Map();
+Students.set(stanli, gorgeLessons);
+
+console.log(`Преподователь по математике: ${lessons.get("Математика")}`);
+console.log(`Уроки Gorge: ${[...Students.get(stanli)]}`);
